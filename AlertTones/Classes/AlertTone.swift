f21c2ddbacebe6a6282854a9bd328706a8f315ad
Duncan Cunningham <duncanc4@gmail.com>
2022-06-10 13:55:24 +0200

    Make AlertTone usable in Objective-C

--- AlertTones/Classes/AlertTone.swift
+++ AlertTones/Classes/AlertTone.swift
@@ -7,8 +7,23 @@
 
 import Foundation
 
-public struct AlertTone: Codable, Equatable {
+public final class AlertTone: NSObject, Codable {
     
     public let name: String
     public let url: URL
+    
+    public init(name: String, url: URL) {
+        self.name = name
+        self.url = url
+        
+        super.init()
+    }
+    
+    public override func isEqual(_ object: Any?) -> Bool {
+        guard let alertTone = object as? AlertTone else {
+            return false
+        }
+        
+        return (self.name == alertTone.name && self.url == alertTone.url)
+    }
 }
